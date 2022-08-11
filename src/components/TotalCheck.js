0a4bac95c017e7a8169ee1c8059036468ff38261
vb <51056367+vitalbeach@users.noreply.github.com>
2022-08-10 20:03:08 -0400

    added input validation

--- src/components/TotalCheck.js
+++ src/components/TotalCheck.js
@@ -1,42 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export class TotalCheck extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         
-      }
+let contains_invalid_input = false;
+function validate_input(input_values) {
+  let invalid_response_count = 0;
+  let total_characters = 0;
+  for (let i of input_values) {
+    if (isNaN(parseInt(i))) {
+      invalid_response_count += 1;
+    } else {
+      total_characters += parseInt(i);
     }
-
-    render() {
-        const { twos, threes, fours, fives, sixes } = this.props
-        let two = parseInt(twos)
-        let three = parseInt(threes)
-        let four = parseInt(fours)
-        let five = parseInt(fives)
-        let six = parseInt(sixes)
-        let total = two + three + four + five + six
-        let totalCheckMessage = ''
-
-        if (total === 10) {
-            totalCheckMessage = <div>Just right!</div>
-        }
-
-        else if (total > 10) {
-            totalCheckMessage = <div>Too much! Remove {total - 10} characters</div>
-        }
-
-        else if (total < 10) {
-            totalCheckMessage = <div>Add {10 - total} characters</div>
-        }
-
-        return (
-            <div>
-                {totalCheckMessage}
-            </div>
-        )
-    }
+  }
+  if (invalid_response_count > 0) {
+    contains_invalid_input = true;
+  } else {
+    contains_invalid_input = false;
+  }
+  return total_characters;
 }
 
-export default TotalCheck
+export class TotalCheck extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    const { twos, threes, fours, fives, sixes } = this.props;
+    let total = validate_input([twos, threes, fours, fives, sixes]);
+    let totalCheckMessage = "";
+
+    if (contains_invalid_input) {
+      totalCheckMessage = <div>Please use integers only</div>;
+    } else if (total === 10) {
+      totalCheckMessage = <div>Just right!</div>;
+    } else if (total > 10) {
+      totalCheckMessage = <div>Too much! Remove {total - 10} characters</div>;
+    } else if (total < 10) {
+      totalCheckMessage = <div>Add {10 - total} characters</div>;
+    }
+
+    return <div>{totalCheckMessage}</div>;
+  }
+}
+
+export default TotalCheck;
